e31d2abf1fe50b6c8c401e0391797b3fcd09f48e
Matt Newbrough <newbs@hotmail.com>
2019-08-08 16:44:29 -0700

    Working Commit for section 4

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
 constructor(private http: HttpClient) { }
 
 login(model: any) {
-  return this.http.post(this.baseUrl, model)
+  return this.http.post(this.baseUrl + 'login', model)
     .pipe(
       map((response: any) => {
         const user = response;
